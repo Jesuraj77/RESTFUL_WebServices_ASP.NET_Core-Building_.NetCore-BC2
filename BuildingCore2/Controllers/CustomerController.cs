9f027c125f7f2fd8b115164a425a0da0bc1d7d3a
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:30:14 +0530

    files added

---
+++ BuildingCore2/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingCore2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        //Create a mapping between for Customer DTO to the Customer Entity using AutoMapper.
+       
+        //Create a Get API method to pull all Customer details from the DB(GetCustomers Api Method)
+        //Use few of the PATCH Operations on the Customer Data.
+
+    }
+}
